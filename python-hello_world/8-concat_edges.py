e9bf5b18972abbab3d6d89db7f58f244737cd81a
hedi <hedihamida98@gmail.com>
2022-09-06 22:34:01 +0100

    new

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = "object-oriented programming with Python"
+str = str.split(' ')[5] + str[6] + str.split(' ')[6] + str[6] + str.split(' ')[12] + str[6] + str.split(' ')[0]
 print(str)
