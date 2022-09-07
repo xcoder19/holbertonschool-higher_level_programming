dd515fcfb6ba3030259f9d92604e44ce93820c61
hedi <hedihamida98@gmail.com>
2022-09-07 22:00:03 +0100

    new

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 a = 97
 while (a <= 122):
-    if ((chr(a)!= 'q') and (chr(a) != 'e')):
-        print(f"{chr(a)}")
+    if ((chr(a) != 'q') and (chr(a) != 'e')):
+        print(f'{chr(a)}')
     a = a+1
