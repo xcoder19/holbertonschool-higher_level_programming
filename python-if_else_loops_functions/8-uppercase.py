a4d950a835013124ef6a398963d3ec0c36e4091a
hedi <hedihamida98@gmail.com>
2022-09-07 23:17:15 +0100

    new

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+def uppercase(str):
+    string=""
+    for i in range(len(str)):
+        if(str[i] >= 'a' and str[i] <= 'z'):
+            string = string + chr((ord(str[i]) - 32))
+
+    print(string)
+
+
