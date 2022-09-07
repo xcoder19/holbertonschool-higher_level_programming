f29b33405060f184b7111f9e361729bb1f1a0997
hedi <hedihamida98@gmail.com>
2022-09-07 22:37:02 +0100

    new

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 a=0
-while(a<=99):
-    if (a >=0) and (a<10) and (a!=99):
-       print(f"0{a}",end=", ")
-    elif(a==99):
-        print(f"{a}",end="")
-    else:
-       print(f"{a}",end=", ")
+while(a<=98):
+    print("{:02d}".format(a) ,end=", ")
     a=a+1
+print(99)
