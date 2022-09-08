6117459bc6407bd51045b2295464ba6a067d2d57
hedi <hedihamida98@gmail.com>
2022-09-08 08:23:52 +0100

    new

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 def pow(a, b):
+    x = b
+    if b < 0:
+        x = -b
     pow = 1
-    while(b != 0):
+    while(x != 0):
         pow = a * pow
-        b = b - 1
+        x = x - 1
+    if (b < 0):
+        return 1 / pow    
     return pow
