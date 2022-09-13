6ba58007337647b274a3fdd57494111fbf21f8e9
hedi <hedihamida98@gmail.com>
2022-09-13 16:25:14 +0100

    new

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -6,5 +6,5 @@ def new_in_list(my_list, idx, element):
         new_list = []
         for x in my_list:
             new_list.append(x)
-        new_list[idx] = element  
+        new_list[idx] = element
     return new_list
