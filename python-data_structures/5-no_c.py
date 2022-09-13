ebefef13e29612fe70f503354adff304fb0653d4
hedi <hedihamida98@gmail.com>
2022-09-13 16:29:50 +0100

    new

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    for x in my_string:
-        if x == 'c' or x == 'C':
-            x = ''
+    for x in range(len(my_string)):
+        if my_string[x] == 'c' or my_string[x] == 'C':
+            my_string[x] = ""
     return my_string
