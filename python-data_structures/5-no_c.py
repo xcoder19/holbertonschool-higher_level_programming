c16eb805432d19b9606cfa97d04f8b7cdf38ed2d
hedi <hedihamida98@gmail.com>
2022-09-13 16:34:56 +0100

    new

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     new_string = ""
     for x in range(len(my_string)):
-       if my_string[x] != 'c' and my_string[x] != 'C':
+        if my_string[x] != 'c' and my_string[x] != 'C':
             new_string = new_string + my_string[x]
     return new_string
