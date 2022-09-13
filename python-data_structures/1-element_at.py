b16864f72fdb66792090aab5d660877db7851951
hedi <hedihamida98@gmail.com>
2022-09-13 14:36:18 +0100

    new

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 def element_at(my_list,idx):
-    if (idx < 0) or (len(my_list) < idx):
+    print(len(my_list))
+    if (len(my_list) < idx) or (idx < 0):
         return
     else:
         return my_list[idx]
+
+
+element_at([1,3,4,5],4)
