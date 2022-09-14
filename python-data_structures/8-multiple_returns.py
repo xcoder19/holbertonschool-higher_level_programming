e35abd41174c301fdb13fa033ec8f3d0f42bed7f
hedi <hedihamida98@gmail.com>
2022-09-14 23:16:30 +0100

    new

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -4,6 +4,7 @@ def multiple_returns(sentence):
     length = len(sentence)
     if length == 0:
         first_character = None
-    first_character = sentence[0]
+    else:
+        first_character = sentence[0]
     tuple = (length,first_character)
     return tuple
