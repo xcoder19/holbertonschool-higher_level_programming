a018505376419eaa15603ecffd37e585aae0a915
hedi <hedihamida98@gmail.com>
2022-09-15 22:32:28 +0100

    new

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
     bool = False
-    for i in a_dictionary.key():
+    for i in a_dictionary.keys():
         if i == key:
             bool = True
             break
