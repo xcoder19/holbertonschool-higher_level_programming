f034e5bd7454e4a1c9a0fed5cb84c0e130d676b4
hedi <hedihamida98@gmail.com>
2022-09-15 21:12:28 +0100

    new

---
+++ python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3 
+def only_diff_elements(set_1, set_2):
+    new_set = []
+    for i in set_1:
+        for x in set_2:
+            if i != x:
+                new_set.append(i)
+    return new_set
