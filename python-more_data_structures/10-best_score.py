6f79099dea0b47388c7f26d09921209861340aef
hedi <hedihamida98@gmail.com>
2022-09-15 22:51:30 +0100

    new

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 def best_score(a_dictionary):
     max = 0
     key = ""
-    if len(a_dictionary) is None:
+    if a_dictionary is None:
         return None
     for i in a_dictionary.keys():
         if a_dictionary[i] > max:
