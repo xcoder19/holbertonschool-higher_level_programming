9f9592cdb4a52e38d0559c2d6cf8604ee546187c
hedi <hedihamida98@gmail.com>
2022-09-15 20:48:35 +0100

    new

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-#!/usr/bin/python3 
-def square_matrix_simple(matrix=[]):
+def square_matrix_simple(matrix = []):
     new_matrix = []
     for i in matrix:
         new_array = []
