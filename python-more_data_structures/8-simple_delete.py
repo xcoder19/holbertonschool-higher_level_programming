4a9bc39d70944a657c3dd7fe53a2b29daea65a9b
hedi <hedihamida98@gmail.com>
2022-09-15 22:31:59 +0100

    new

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    a_dictionary.pop(key)
+    bool = False
+    for i in a_dictionary.key():
+        if i == key:
+            bool = True
+            break
+    if bool is True:
+        a_dictionary.pop(key)
     return a_dictionary
