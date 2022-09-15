9c585ddf6db20a4b08433e05bef1cda83d67a4b0
hedi <hedihamida98@gmail.com>
2022-09-15 22:06:51 +0100

    new

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-
 def print_sorted_dictionary(a_dictionary):
-    return a_dictionary.sort()
+    for i in sorted(a_dictionary.keys()):
+        print("{}: {}".format(i, a_dictionary[i]))
