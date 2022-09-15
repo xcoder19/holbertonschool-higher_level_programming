273e90539c2309654f4c2b28f6cfc3ccf566d07a
hedi <hedihamida98@gmail.com>
2022-09-15 21:20:30 +0100

    new

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,8 +1,19 @@
 #!/usr/bin/python3 
 def only_diff_elements(set_1, set_2):
     new_set = []
+    for i in set_2:
+        bool = False
+        for x in set_1:
+            if i == x:
+                bool = True
+        if bool is False:
+            new_set.append(i)
+
     for i in set_1:
+        bool = False
         for x in set_2:
-            if i != x:
-                new_set.append(i)
+            if i == x:
+                bool = True
+        if bool is False:
+            new_set.append(i)         
     return new_set
