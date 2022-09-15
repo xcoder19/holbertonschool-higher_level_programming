251a62f7b1571bae0fd800cf38d962b72c6f7523
hedi <hedihamida98@gmail.com>
2022-09-15 21:21:21 +0100

    new

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -15,5 +15,5 @@ def only_diff_elements(set_1, set_2):
             if i == x:
                 bool = True
         if bool is False:
-            new_set.append(i) 
+            new_set.append(i)
     return new_set
