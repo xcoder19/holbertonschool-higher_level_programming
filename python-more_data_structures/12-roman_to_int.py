660a77143cd92c6eeccab99d4a5f4c36d9f7d9f8
hedi <hedihamida98@gmail.com>
2022-09-16 18:48:03 +0100

    new

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+from distutils.core import run_setup
+
+
+def roman_to_int(roman_string):
+    if roman_string is None or str(roman_string) is False:
+        return 0
+    result = 0
+    romman = {
+    "I": 1,
+    "V": 5,
+    "X": 10,
+    "L": 50,
+    "C": 100,
+    "D": 500,
+    "M": 1000
+ }
+    for x in roman_string:
+        for i in romman:
+            if (i == x):
+                result = result + romman[i]
+
+    if (len(roman_string) >= 2):
+        if (roman_string != "III" and roman_string != "II" and roman_string[0] == 'I' and roman_string[1] != 'X' and roman_string[1] != 'V'):
+            print('hello')
+            result = result - 1
+
+    if (len(roman_string) >= 2 and roman_string[0] == 'I' and roman_string[1] == 'X'):
+        print('hello2')
+        result = result - 2
+
+   
+    if (len(roman_string) >= 2 and roman_string[0] == 'I' and roman_string[1] == 'V'):
+        print('hello3')
+        result = result - 2
+    print(result)
