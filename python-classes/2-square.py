6eecaa294ddcb505885c5f19d9bbabcf92b4cd5d
hedi <hedihamida98@gmail.com>
2022-09-21 12:21:20 +0100

    new

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -5,9 +5,8 @@
 class Square:
     """class square"""
     def __init__(self, size=0):
-        if  isinstance(size, int) == False:
+        if isinstance(size, int) is False:
             raise TypeError("size must be an integer")
         if size < 0:
             raise TypeError("size must be >= 0")
-        
         self.__size = size
