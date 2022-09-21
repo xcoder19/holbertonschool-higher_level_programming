3160dafefbd1582ae355fd5d97a9914e2ed521ce
hedi <hedihamida98@gmail.com>
2022-09-21 16:41:57 +0100

    new

---
+++ python-classes/5-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""class square"""
+
+
+class Square:
+    """class square"""
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+            return self.__size
+
+    @size.setter
+    def size(self, size):
+        if isinstance(size, int) is False:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise TypeError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
+
+    def my_print(self):
+        if(self.__size == 0):
+            print('')
+        for x in range(self.__size):
+            for i in range(self.__size):
+                print("#", end="")
+        print('')
