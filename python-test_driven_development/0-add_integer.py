10976d031835d688cd566702e196cc2c82cff00e
hedi <hedihamida98@gmail.com>
2022-09-22 22:59:25 +0100

    new

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+def add_integer(a, b=98):
+    if not type(a) in [int, float]:
+        raise TypeError("a must be an integer")
+    if not type(b) in [int, float]:
+        raise TypeError("b must be an integer")
+    a = int(a)
+    b = int(b)
+    return a + b
