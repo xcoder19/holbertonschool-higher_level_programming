0404077a6063270d87086ecf3e444ffb379958fc
hedi <hedihamida98@gmail.com>
2022-09-22 21:16:11 +0100

    new

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""""Rectangle class"""
+
+class Rectangle:
+    """"Rectangle class"""
+    def __init__(self, width=0, height=0):
+        self.height = height
+        self.width = width
+        
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        if not isinstance(width, int):
+            raise TypeError('width must be an integer')
+        if width < 0:
+            raise ValueError('width must be >= 0')
+        self.__width = width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        if not isinstance(height, int):
+            raise TypeError('height must be an integer')
+        if height < 0:
+            raise ValueError('height must be >= 0')
+        self.__height = height
