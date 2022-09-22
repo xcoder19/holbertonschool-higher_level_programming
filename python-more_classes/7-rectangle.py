968810889d5132c935a184376e468bfed4712df9
hedi <hedihamida98@gmail.com>
2022-09-22 22:20:13 +0100

    new

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -6,6 +6,7 @@ class Rectangle:
     """"Rectangle class"""
     number_of_instances = 0
     print_symbol = '#'
+
     def __init__(self, width=0, height=0):
         Rectangle.number_of_instances = Rectangle.number_of_instances + 1
         self.height = height
