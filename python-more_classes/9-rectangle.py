0bf53e259c1ed2da0b0ebee7f75198bbacf8e693
hedi <hedihamida98@gmail.com>
2022-09-22 22:39:12 +0100

    new

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -50,7 +50,7 @@ class Rectangle:
         if not isinstance(height, int):
             raise TypeError('height must be an integer')
         if height < 0:
-            raise ValueError('height must be >= 0')
+            raise ValueError('width must be >= 0')
         self.__height = height
 
     def area(self):
