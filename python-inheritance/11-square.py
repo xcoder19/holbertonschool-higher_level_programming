2a3dd347950d55d386b01f8c5b87361f908ffc3b
hedi <hedihamida98@gmail.com>
2022-09-29 20:55:31 +0100

    new

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""""Square class"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """"Square class"""
+
+    def __init__(self, size):
+
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
+    
+    def __str__(self):
+        return f"[Square] {self.__size}/{self.__size}"
