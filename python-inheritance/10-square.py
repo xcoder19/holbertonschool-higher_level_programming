d33b530a66b386981656e80dc6c3ab6493ea9e78
hedi <hedihamida98@gmail.com>
2022-09-29 20:52:57 +0100

    new

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""""Square class"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """"Square class"""
+
+    def __init__(self, size):
+        
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
