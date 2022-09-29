dc71a026b758d95c7d2ba90cecf632c345c3b438
hedi <hedihamida98@gmail.com>
2022-09-29 20:38:03 +0100

    new

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""basegeometry class"""
+
+
+
+class BaseGeometry:
+    """basegeometry class"""
+    
+    def area(self):
+        raise Exception("area() is not implemented")
