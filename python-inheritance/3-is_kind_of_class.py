bb448ff070243874afc3c52f7d748872c828b4f1
hedi <hedihamida98@gmail.com>
2022-09-29 20:30:34 +0100

    new

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,4 +1,4 @@
-#!/bin/usr/python3
+#!/usr/bin/python3
 """func returns if an obj is an instance of a class"""
 def is_kind_of_class(obj, a_class):
     """func returns if an obj is an instance of a class"""
