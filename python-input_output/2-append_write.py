9cd857ae1a54ab0c67bb583f6f980951ebcdefa9
hedi <hedihamida98@gmail.com>
2022-10-05 21:34:24 +0100

    new

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+#!/usr/bin/python3
+
+""" append file func"""
+
+
+def append_write(filename="", text=""):
+    """ append file func"""
+
+    with open(filename, 'a') as file:
+        return file.write(text)
