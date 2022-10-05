80e1966a8c3e74dca978a8b4992de1d99f0b6eb3
hedi <hedihamida98@gmail.com>
2022-10-05 21:56:33 +0100

    new

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -6,6 +6,6 @@ def save_to_json_file(my_obj, filename):
     if not isinstance(my_obj,set):
         text = json.dumps(my_obj)
     else:
-        text = ""
+        text = {}
     with open(filename, 'w') as file:
         file.write(text)
