267faa5c273939c4732c015f1a6463dbf5a3097a
hedi <hedihamida98@gmail.com>
2022-10-05 21:31:25 +0100

    new

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -5,8 +5,7 @@
 
 def write_file(filename="", text=""):
     """ write file func"""
-    a = 0
+    
     with open(filename, 'a') as file:
         file.write(text)
-        a = a + 1
-    return a
+        return len (file)
