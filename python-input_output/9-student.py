1a1fe054f4807fb6cb0df1a3268a0de22f4fff8e
hedi <hedihamida98@gmail.com>
2022-10-06 20:13:49 +0100

    new

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -2,11 +2,9 @@
 """student class"""
 
 
-
 class Student:
     """student class"""
 
-    
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
