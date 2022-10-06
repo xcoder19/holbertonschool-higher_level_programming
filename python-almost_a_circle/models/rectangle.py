d31f7bd2029588a09abd1fcfad0d584c6785042a
hedi <hedihamida98@gmail.com>
2022-10-06 21:54:57 +0100

    new

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -73,4 +73,5 @@ class Rectangle(Base):
             print()
 
     def __str__(self):
-        return(f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}")
+        return(f"[Rectangle] ({self.id}) {self.x}/{self.y} - \
+        {self.width}/{self.height}")
