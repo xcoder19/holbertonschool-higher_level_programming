10e70f60bb96cc2651d121cae6f82d81619c59f9
hedi <hedihamida98@gmail.com>
2022-10-06 22:34:52 +0100

    new

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""square class"""
+from models.rectangle import Rectangle
+
+class Square(Rectangle):
+    """square class"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        return f"[Square] ({super().id}) {super().__x}/{super().__y} - {super().height}"
