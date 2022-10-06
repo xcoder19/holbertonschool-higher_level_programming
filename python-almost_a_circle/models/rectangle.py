eb770938bee24675a83c518cfa9d6a6294bd7784
hedi <hedihamida98@gmail.com>
2022-10-06 22:12:51 +0100

    new

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -67,13 +67,13 @@ class Rectangle(Base):
 
     def display(self):
         """display function"""
-        
+
         if(self.__y > 0):
                 print()
         for x in range(self.height):
             if (self.__x > 0):
-                print(' ',end="")
-            
+                print(' ', end="")
+ 
             for i in range(self.width):
                 print('#', end="")
             print()
