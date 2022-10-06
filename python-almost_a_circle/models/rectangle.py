f7c9d24c6087e1090965216f6273e75597f86b53
hedi <hedihamida98@gmail.com>
2022-10-06 22:52:35 +0100

    new

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -95,4 +95,7 @@ class Rectangle(Base):
         else:
             for x in kwargs:
                 setattr(self, x, kwargs[x])
-                i += 1
+                
+    def to_dictionary(self):
+        return {'id':self.id, 'width':self.width,
+                'height':self.height, 'x':self.x, 'y':self.y}
