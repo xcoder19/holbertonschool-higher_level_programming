b0f72e7b91230c57b7fa17ad1a369bc8787a93bc
hedi <hedihamida98@gmail.com>
2022-10-06 23:44:02 +0100

    new

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -49,6 +49,6 @@ class Base:
     @classmethod
     def create(cls, **dictionary):
         """"create function"""
-        obj = cls(1,2)
+        obj = cls(1,2,1,1)
         obj.update(**dictionary)
         return (obj)
