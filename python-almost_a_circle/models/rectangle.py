a3f241e111793b0c3fc37c72e1c0ecd50074730a
hedi <hedihamida98@gmail.com>
2022-10-06 22:53:46 +0100

    new

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -98,6 +98,6 @@ class Rectangle(Base):
                 
     def to_dictionary(self):
         """ to_dictionary func"""
-        
-        return {'id':self.id, 'width':self.width,
-                'height':self.height, 'x':self.x, 'y':self.y}
+
+        return {'id': self.id, 'width': self.width,
+                'height': self.height, 'x': self.x, 'y': self.y}
