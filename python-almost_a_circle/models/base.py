ab5b2a726713fd678c3b0f487602716d1dbc992b
hedi <hedihamida98@gmail.com>
2022-10-06 20:41:25 +0100

    new

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+class Base:
+    __nb_objects = 0
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            self.__nb_objects += 1
+            self.id = self.__nb_objects
