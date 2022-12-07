3fca74471cc68506e7eda13809b148bf9bc17ad0
hedi <hedihamida98@gmail.com>
2022-12-07 19:35:34 +0100

    arguments and log

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const myArgs = process.argv.slice(2);
-if (myArgs.length == 0) {
+if (myArgs.length === 0) {
 console.log('No argument');
-} else if (myArgs.length == 1) {
+} else if (myArgs.length === 1) {
 console.log('Argument found');
 } else {
 console.log('Arguments found');
