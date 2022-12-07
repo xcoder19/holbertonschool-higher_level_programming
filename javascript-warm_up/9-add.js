4e21b4d3e7e6325a146f5682c8b04f650f4464da
hedi <hedihamida98@gmail.com>
2022-12-07 20:38:20 +0100

    add func

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 const myArgs = process.argv.slice(2);
-function add(a, b) {
-return a + b;
+function add (a, b) {
+  return a + b;
 }
 if (parseInt(myArgs[0]) && parseInt(myArgs[1])) {
-console.log(add(parseInt(myArgs[0]), parseInt(myArgs[1])));
+  console.log(add(parseInt(myArgs[0]), parseInt(myArgs[1])));
 } else {
-console.log(NaN);
+  console.log(NaN);
 }
