d099162112add33e8aac14d0e939a429ca6eaa24
hedi <hedihamida98@gmail.com>
2022-12-07 20:14:57 +0100

    while loop

---
+++ javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+const myArgs = process.argv.slice(2);
+
+let i = 0;
+if (parseInt(myArgs[0])) {
+  while (i < parseInt(myArgs[0])) {
+    console.log('C is fun');
+    i++;
+  }
+} else {
+  console.log('Missing number of occurrences');
+}
