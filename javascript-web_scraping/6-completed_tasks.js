adb6157f0d4ded9c240465a4825625737f7befaf
hedi <hedihamida98@gmail.com>
2022-12-12 12:53:38 +0100

    fetch data and print object

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -4,6 +4,10 @@ const myArgs = process.argv.slice(2);
 request(myArgs[0], function (error, response, body) {
   if (!error) {
     const obj = {};
+    let arr = [];
+    JSON.parse(body).forEach(task => { 
+        console.log(task.userId);
+    })
     for (let id = 1; id <= 10; id++) {
       let count = 0;
       JSON.parse(body).forEach(element => {
@@ -13,7 +17,7 @@ request(myArgs[0], function (error, response, body) {
       });
       obj[id] = count;
     }
-    console.log(obj);
+    //console.log(obj);
   } else {
     console.log(404);
   }
