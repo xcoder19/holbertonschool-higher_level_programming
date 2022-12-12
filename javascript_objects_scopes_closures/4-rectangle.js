c7828f08e18f6d4230092668717542b8ac787d52
hedi <hedihamida98@gmail.com>
2022-12-12 01:58:04 +0100

    rec  class

---
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let square = '';
+    for (let i = 0; i < this.width; i++) {
+      square += 'X';
+    }
+    for (let j = 0; j < this.height; j++) {
+      console.log(square);
+    }
+  }
+
+  rotate () {
+    const tmp = this.height;
+    this.height = this.width;
+    this.width = tmp;
+  }
+
+  double () {
+    this.height *= 2;
+    this.width *= 2;
+  }
+};
