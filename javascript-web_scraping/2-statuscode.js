c31e37e922f70ebfd63f23ec5efb1f7cf0067691
hedi <hedihamida98@gmail.com>
2022-12-12 11:36:09 +0100

    request and print status code

---
+++ javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const request = require('request');
+const myArgs = process.argv.slice(2);
+
+request(myArgs[0], function (error, response, body) {
+  if (!error) {
+    console.log(response.statusCode);
+  } else {
+    console.log(404);
+  }
+});
