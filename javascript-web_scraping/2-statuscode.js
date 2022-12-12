88655a1c59cf6f0c1d561ebbcab2d61d33911e63
hedi <hedihamida98@gmail.com>
2022-12-12 11:38:11 +0100

    request and print status code

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-const request = require('request');
+const request = require('request');
 const myArgs = process.argv.slice(2);
 
-request(myArgs[0], function (error, response, body) {
+request(myArgs[0], function (error, response, body) {
   if (!error) {
     console.log(`code: ${response.statusCode}`);
   } else {
