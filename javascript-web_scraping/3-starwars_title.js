94069504d487b614585bc51c1db6c88fc8836a06
hedi <hedihamida98@gmail.com>
2022-12-12 12:10:08 +0100

    title

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,18 +1,10 @@
 #!/usr/bin/node
-
 const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/';
+const myArgs = process.argv.slice(2);
+const url = `https://swapi-api.hbtn.io/api/films/${myArgs[0]}`;
 request(url, function (error, response, body) {
   if (!error) {
-    let count = 0;
-    JSON.parse(response.body).results.forEach(element => {
-      element.characters.forEach((char) => {
-        if (char.includes('18')) {
-          count++;
-        }
-      });
-    });
-    console.log(count);
+    console.log(JSON.parse(response.body).title);
   } else {
     console.log(404);
   }
