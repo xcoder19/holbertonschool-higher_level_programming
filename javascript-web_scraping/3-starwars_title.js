530ecb7ec7ef5907cabe94b13e607f69762c700d
hedi <hedihamida98@gmail.com>
2022-12-12 11:44:59 +0100

    json parse response

---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const request = require('request');
+const myArgs = process.argv.slice(2);
+const url = `https://swapi-api.hbtn.io/api/films/${myArgs[0]}`;
+request(url, function (error, response, body) {
+  if (!error) {
+    console.log(JSON.parse(response.body).title);
+  } else {
+    console.log(404);
+  }
+});
